c610861f937ebf187c1da0f9b62d66ca96e1b5dd
Nishant <nishantparmar@gmail.com>
2022-02-20 09:27:13 -0500

    adding

--- src/main/java/com/nishant/problems/misc/RandomInteger.java
+++ src/main/java/com/nishant/problems/misc/RandomInteger.java
@@ -1,10 +1,23 @@
 package com.nishant.problems.misc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RandomInteger {
 
     public static int getRandom(int[] array) {
         int rnd = (int)(Math.random()*array.length);
         return array[rnd];
+    }
+
+    Map<Integer, Integer> map = new HashMap();
+    List list = new ArrayList();
+
+    public int getRandom() {
+        int rnd = (int)(Math.random()*list.size());
+        return map.get(rnd);
     }
 
     public static void main(String[] args) {
